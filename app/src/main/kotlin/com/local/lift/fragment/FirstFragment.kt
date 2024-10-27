98cb1281feeb3d098989a099e514da13045f8cb6
Chansovanmony Yoeun <51309276+Monyjenni@users.noreply.github.com>
2024-10-27 14:43:35 +0700

    re-structre directories & verified_screen

--- app/src/main/kotlin/com/local/lift/fragment/FirstFragment.kt
+++ app/src/main/kotlin/com/local/lift/fragment/FirstFragment.kt
@@ -1,4 +1,4 @@
-package com.local.lift
+package com.local.lift.fragment
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
+import com.local.lift.R
 import com.local.lift.databinding.FragmentFirstBinding
 
 class FirstFragment : Fragment() {
