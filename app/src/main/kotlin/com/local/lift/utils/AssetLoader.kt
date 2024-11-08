e7a752fa1f53d5e112efb07d5d9d72d439f37b3e
Chansovanmony Yoeun <51309276+Monyjenni@users.noreply.github.com>
2024-11-08 11:18:41 +0700

    feat: Separate users json data & asset util

---
+++ app/src/main/kotlin/com/local/lift/utils/AssetLoader.kt
@@ -0,0 +1,16 @@
+package com.local.lift.util
+
+import android.content.Context
+import com.google.gson.Gson
+import com.local.lift.model.SignInResponse
+import java.io.InputStreamReader
+
+object AssetLoader {
+    fun loadUsersFromAssets(context: Context): SignInResponse {
+        val inputStream = context.assets.open("users.json")
+        val reader = InputStreamReader(inputStream)
+
+        // Use Gson to parse the JSON file into a SignInResponse object
+        return Gson().fromJson(reader, SignInResponse::class.java)
+    }
+}
