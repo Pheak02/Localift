fabce96758d13bf3602770e285d00d2965204e60
Chansovanmony Yoeun <51309276+Monyjenni@users.noreply.github.com>
2024-12-13 09:47:35 +0700

    feat: Add all screens navigation

--- app/src/main/kotlin/com/local/lift/fragment/Category1Fragment.kt
+++ app/src/main/kotlin/com/local/lift/fragment/Category1Fragment.kt
@@ -5,18 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.local.locallift.databinding.ProductBinding
+import com.local.locallift.databinding.Category1Binding
 
-class ProductFragment : Fragment() {
+class Category1Fragment : Fragment() {
 
-    private var _binding: ProductBinding ? = null
+    private var _binding: Category1Binding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = ProductBinding.inflate(inflater, container, false)
+        _binding = Category1Binding.inflate(inflater, container, false)
         return binding.root
     }
 
