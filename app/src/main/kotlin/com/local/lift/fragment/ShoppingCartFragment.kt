cdbd2618664d43fe6b79d54b0b2ef79f83751737
Chansovanmony Yoeun <51309276+Monyjenni@users.noreply.github.com>
2025-01-01 13:30:45 +0700

    feat:Add checkout.xml

--- app/src/main/kotlin/com/local/lift/fragment/ShoppingCartFragment.kt
+++ app/src/main/kotlin/com/local/lift/fragment/ShoppingCartFragment.kt
@@ -24,7 +24,7 @@ class ShoppingCartFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.shoppingCart.setOnClickListener {
+        binding.checkoutButton.setOnClickListener {
             findNavController().navigate(R.id.action_shoppingCartFragment_to_checkoutFragment)
         }
     }
